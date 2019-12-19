a49011a87b017915064b9c1f008d4432c9f74428
Rohan Rajput <rohanrajput32843@gmail.com>
2019-12-20 00:42:00 +0530

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,27 @@ import Navbar from './Components/Layout/Navbar';
 import Landing from './Components/Layout/Landing';
 import Login from './auth/Login';
 import Register from './auth/Register';
+import Alert from './Components/Layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 const App=()=>
+      <Provider store={store}>
          <Router> 
             <Fragment>
                     <Navbar />
                     <Route exact path='/' component={Landing}/>
                     <section className="container">
+                        <Alert />
                         <Switch>
                            <Route exact path='/register' component={Register}/>
                            <Route exact path='/login' component={Login}/>
                         </Switch>
                     </section>
             </Fragment>
-         </Router>   
+         </Router> 
+      </Provider>     
 
 export default App;
--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,7 +1,10 @@
 import React,{Fragment,useState} from 'react'
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {setAlert} from '../actions/alert';
+import PropTypes from 'prop-types'; 
 
-const Register = () => {
+const Register = ({setAlert}) => {
     const [formData,setFormData]=useState({
         name:'',
         email:'',
@@ -19,7 +22,7 @@ const Register = () => {
         e.preventDefault();
         if(password!==password2)
         {
-            console.log('password do not match')
+             setAlert('password do not match','danger');
         }
         else{
             console.log(formData);
@@ -78,4 +81,8 @@ const Register = () => {
     )
 }
 
-export default Register
+Register.propTypes={
+    setAlert:PropTypes.func.isRequired,
+}
+
+export default connect(null,{ setAlert })(Register);
