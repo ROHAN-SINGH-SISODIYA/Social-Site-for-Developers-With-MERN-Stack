fea483cd58a92d3542c8f60e2779318e853ff64a
Rohan Rajput <rohanrajput32843@gmail.com>
2019-12-22 01:38:28 +0530

    user loaded

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React,{Fragment} from 'react';
+import React,{Fragment,useEffect} from 'react';
 import './App.css';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import Navbar from './Components/Layout/Navbar';
@@ -9,9 +9,20 @@ import Alert from './Components/Layout/Alert';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import {loadUser} from './actions/auth';
 
+if(localStorage.token){
+      setAuthToken(localStorage.token);
+    }
 
-const App=()=>
+const App=()=>{
+   
+   useEffect(()=>{
+      store.dispatch(loadUser());
+   },[]);
+
+   return(
       <Provider store={store}>
          <Router> 
             <Fragment>
@@ -27,5 +38,5 @@ const App=()=>
             </Fragment>
          </Router> 
       </Provider>     
-
+   )};
 export default App;
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
 } from '../actions/types';
 
 const initialState={
@@ -13,6 +15,13 @@ const initialState={
 export default function(state=initialState,action){
     const {type,payload} =action;
     switch(type){
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated:true,
+                loading:false,
+                user:payload
+            }
         case REGISTER_SUCCESS:
             localStorage.setItem('token',payload.token);
             return {
@@ -22,6 +31,7 @@ export default function(state=initialState,action){
                 loading:false
             }
         case REGISTER_FAIL:
+        case AUTH_ERROR:
             localStorage.removeItem('token');
             return {
                 ...state,
