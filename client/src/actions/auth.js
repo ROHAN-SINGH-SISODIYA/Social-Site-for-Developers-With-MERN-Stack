216b3b8b8c0a7bc2b98ec3fe1853edc38be657c4
Rohan Rajput <rohanrajput32843@gmail.com>
2019-12-22 15:58:02 +0530

    login done

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,9 +4,14 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
+
+
+
 // Load user
 export const loadUser =() => async dispatch=>{
     if(localStorage.token)
@@ -45,7 +50,8 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
-
+    dispatch(loadUser());
+    
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -59,5 +65,38 @@ export const register = ({ name, email, password }) => async dispatch => {
   }
 };
 
+
+
+// Login User
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+      
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
 
 
